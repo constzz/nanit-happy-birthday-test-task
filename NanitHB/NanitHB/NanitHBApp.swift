7265d8d58f4fec4b4854102d8324017f8f92e67c
Konstantin <77053754+constzz@users.noreply.github.com>
2025-09-20 15:53:56 +0300

    Xcode project init

---
+++ NanitHB/NanitHB/NanitHBApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanitHBApp.swift
+//  NanitHB
+//
+//  Created by Konstantin Bezzemelnyi on 20.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct NanitHBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
